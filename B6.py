21842d6b06ffaa28c3eb7ffdae3f7b33c472ace4
Huyptg1809 <huy.ptg.media@gmail.com>
2026-05-10 06:56:48 +0700

    Auto upload by HuyTool

---
+++ B6.py
@@ -0,0 +1,7 @@
+so_thap_phan = input("Nhập một số thập phân: ")
+
+so_float = float(so_thap_phan)
+
+so_lam_tron = round(so_float)
+
+print("Số sau khi làm tròn và chuyển thành số nguyên:", int(so_lam_tron))
